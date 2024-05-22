82fdf546aab8e099808d52a507b45d2e79a0a759
Kyle Banderman <kyle.banderman4675@gmail.com>
2024-05-21 19:53:22 -0500

    added start of data

---
+++ backend/DBDRandomAndData/src/main/java/org/kraken/DBDRandomAndData/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package org.kraken.DBDRandomAndData.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/home")
+public class HomeController {
+
+    public ResponseEntity<String> randomizeKillerBuild () {
+
+    }
+}
